82ce94e507647a86c8ba6e2197e0f9853ec684c5
Jason Tavera <taverajason7@gmail.com>
2023-08-10 22:47:27 -0500

    created a todo jsx file and created a function inside returning a div with some text and a button

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div>
+      <h1>My todo list</h1>
+      <Todo/>
+      <Todo/>
+      <Todo/>
+      </div>
+  );
+}
+
+export default App;
